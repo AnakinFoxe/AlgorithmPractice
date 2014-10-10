78b08c0ea65dc3eb111debc491a312377ed4c7b7
AnakinFoxe <xing.y.hu@gmail.com>
2014-10-09 21:01:34 -0700

    Set targets for week1

---
+++ src/main/java/edu/csupomona/algo/part1/wk1/ClosestPair2D.java
@@ -0,0 +1,7 @@
+package edu.csupomona.algo.part1.wk1;
+
+/**
+ * Created by Xing HU on 10/9/14.
+ */
+public class ClosestPair2D {
+}
---
+++ src/main/java/edu/csupomona/algo/part1/wk1/InsertionSort.java
@@ -0,0 +1,7 @@
+package edu.csupomona.algo.part1.wk1;
+
+/**
+ * Created by Xing HU on 10/8/14.
+ */
+public class InsertionSort {
+}
