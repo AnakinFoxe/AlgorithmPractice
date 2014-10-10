9cff1cf40606390c0ab3515f2ad76e656fb7e51b
AnakinFoxe <xing.y.hu@gmail.com>
2014-10-10 00:39:15 -0700

    Finished BubbleSort

--- src/main/java/edu/csupomona/algo/part1/wk1/BubbleSort.java
+++ src/main/java/edu/csupomona/algo/part1/wk1/BubbleSort.java
@@ -2,31 +2,113 @@ package edu.csupomona.algo.part1.wk1;
 
 import edu.csupomona.algo.common.ListHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Bubble Sort
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1)
  * Created by Xing HU on 10/8/14.
  */
 public class BubbleSort {
 
+    private final ListHelper LH_;
+
+    public BubbleSort() {
+        LH_ = new ListHelper();
+    }
+
     /**
-     * Sort the input list using Bubble Sort
+     * The most naive Bubble Sort
      * @param list          Input list
      */
-    public void sort(List<Integer> list) {
+    public void sort1(List<Integer> list) {
+        int count = 0;
         // double for-loop and both are related with variable "end"
         // O(n^2)
-        for (int end = list.size(); end > 0; --end) {
+        int end = list.size();
+        boolean swapped;
+        do {
+            swapped = false;
             for (int idx = 0; idx < end - 1; ++idx) {
                 int left = list.get(idx);
                 int right = list.get(idx + 1);
                 if (left > right) {
                     list.set(idx + 1, left);
                     list.set(idx, right);
+
+                    swapped = true;
                 }
+
+                ++count;    // loop time
             }
+        } while (swapped);
+
+        LH_.checkNaive(list);
+        System.out.println("Looped " + count + " times");
+    }
+
+    /**
+     *  Optimized by early cutting
+     * @param list              Input list
+     */
+    public void sort2(List<Integer> list) {
+        int count = 0;
+        for (int end = list.size(); end > 0; --end) {
+            boolean swapped = false;
+            for (int idx = 0; idx < end - 1; ++idx) {
+                int left = list.get(idx);
+                int right = list.get(idx + 1);
+                if (left > right) {
+                    list.set(idx + 1, left);
+                    list.set(idx, right);
+
+                    swapped = true;
+                }
+
+                ++count;    // loop time
+            }
+
+            if (!swapped)
+                break;
         }
+
+        LH_.checkNaive(list);
+        System.out.println("Looped " + count + " times");
+    }
+
+    /**
+     * Slightly more optimized by ignoring already sorted part
+     * @param list              Input list
+     */
+    public void sort3(List<Integer> list) {
+        int count = 0;
+        int end = list.size();
+        while (end > 0) {
+            boolean swapped = false;
+            int lastSwap = 0;
+            for (int idx = 0; idx < end - 1; ++idx) {
+                int left = list.get(idx);
+                int right = list.get(idx + 1);
+                if (left > right) {
+                    list.set(idx + 1, left);
+                    list.set(idx, right);
+
+                    swapped = true;
+                    lastSwap = idx + 1;
+                }
+
+                ++count;    // loop time
+            }
+            end = lastSwap;
+
+            if (!swapped)
+                break;
+        }
+
+        LH_.checkNaive(list);
+        System.out.println("Looped " + count + " times");
     }
 
 
@@ -34,9 +116,10 @@ public class BubbleSort {
         ListHelper lh = new ListHelper();
         BubbleSort bs = new BubbleSort();
 
-        List<Integer> list = lh.genNaive(100);
+        List<Integer> list = lh.genNaive(1000);
 
-        bs.sort(list);
-        lh.checkNaive(list);
+        bs.sort1(new ArrayList<>(list));
+        bs.sort2(new ArrayList<>(list));
+        bs.sort3(new ArrayList<>(list));
     }
 }
